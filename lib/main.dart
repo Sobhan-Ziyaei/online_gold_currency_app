e3cf5f3eb503d8cb53f3d6b29dd55db9dfb1f19f
SohanZiyaei <sobhan.ziaee73@gmail.com>
2023-06-23 20:49:13 +0430

    added fonts and localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +12,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Localizations Sample App',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa'), // farsi
+      ],
       home: Scaffold(
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
           actions: [
+            Image.asset('assets/images/currency.png'),
             const SizedBox(
-              width: 8,
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Image.asset('assets/images/menu.png'),
-              ),
+              width: 10,
             ),
             const Align(
               alignment: Alignment.centerRight,
@@ -32,13 +37,19 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 16,
+                    fontFamily: 'sb',
                     fontWeight: FontWeight.bold),
               ),
             ),
-            const SizedBox(
-              width: 18,
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Image.asset('assets/images/menu.png'),
+              ),
             ),
-            Image.asset('assets/images/currency.png'),
+            const SizedBox(
+              width: 8,
+            ),
           ],
         ),
         body: Padding(
@@ -49,19 +60,20 @@ class MyApp extends StatelessWidget {
                 height: 23,
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
+                  Image.asset('assets/images/icon_question.png'),
+                  const SizedBox(
+                    width: 8,
+                  ),
                   const Text(
                     'نرخ آزاد ارز چیست ؟ ',
                     style: TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
+                        fontFamily: 'sm',
                         fontSize: 16),
                   ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Image.asset('assets/images/icon_question.png'),
                 ],
               ),
               const SizedBox(
@@ -69,7 +81,8 @@ class MyApp extends StatelessWidget {
               ),
               const Text(
                 ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند',
-                style: TextStyle(color: Colors.black, fontSize: 13),
+                style: TextStyle(
+                    color: Colors.black, fontSize: 13, fontFamily: 'sm'),
                 textAlign: TextAlign.justify,
                 textDirection: TextDirection.rtl,
               )
